b102526f4e9e0821fe8fdf72efce82447e6e2327
Olivia Follin <odm.follin@gmail.com>
2019-03-11 13:34:45 +0100

    Destroy platforms to same performance
    When generating sections (platforms) they will be destroyed from the scene when their not in sceen

--- 2D Project FG/Assets/Level Generator/Script/LevelGenerator.cs
+++ 2D Project FG/Assets/Level Generator/Script/LevelGenerator.cs
@@ -6,19 +6,30 @@ using UnityEngine;
 public class LevelGenerator : MonoBehaviour
 {
     [SerializeField] private GameObject[] _platforms;
-    [SerializeField] private Transform _cameraPoint;
+    [SerializeField] private Transform _GenerationPoint;
 
-    private float _platformHight = 20;
+    private float _platformHight;
 
-
-    void Start()
+    private void Start()
     {
+        if (_GenerationPoint == null)
+        {
+            Debug.LogError("There is no Generation Point in the scene.");
+            return;
+        }
+
+        if (_platforms.Length <= 0)
+        {
+            Debug.LogError("The array in " + this + " is null. Add Platforms to the array to generate platforms.");
+            return;
+        }
+
         _platformHight = _platforms[0].transform.localScale.y;
     }
 
-    void Update()
+    private void Update()
     {
-        if (transform.position.y < _cameraPoint.transform.position.y)
+        if (transform.position.y < _GenerationPoint.transform.position.y)
         {
             int randomNumber = Random.Range(0, _platforms.Length);
 
---
+++ 2D Project FG/Assets/Level Generator/Script/PlatformDestroyer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformDestroyer : MonoBehaviour
+{
+    private GameObject _platformDestructionPoint;
+
+    private void Start()
+    {
+        if (!GameObject.Find("Platform Destruction Point"))
+        {
+            Debug.LogError("There is no Destruction Point in the scene. Make sure the name of the point is: Platform Destruction Point");
+            return;
+        }
+
+        _platformDestructionPoint = GameObject.Find("Platform Destruction Point");
+    }
+
+    private void Update()
+    {
+        if(_platformDestructionPoint == null) return;
+
+        if (transform.position.y < _platformDestructionPoint.transform.position.y)
+            Destroy(gameObject);
+    }
+
+}
