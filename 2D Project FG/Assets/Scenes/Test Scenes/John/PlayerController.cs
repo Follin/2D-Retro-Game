2a223eaa8de9442201460554fa99ea5d61d44399
Olivia Follin <odm.follin@gmail.com>
2019-03-13 15:54:01 +0100

    Created 8 sections

--- 2D Project FG/Assets/Scenes/Test Scenes/John/PlayerController.cs
+++ 2D Project FG/Assets/Scenes/Test Scenes/John/PlayerController.cs
@@ -4,30 +4,36 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
+    [Header("Player Setup")]
     [SerializeField] private int _index;
-    public float speed;
-    Rigidbody2D _rb;
-    public PlayerController otherPlayer;
-    [SerializeField] private bool specialIsActivated;
-    [SerializeField] private bool specialWaiting;
-    private bool canTransfer;
-    public float transferCooldown;
-    public GameObject abilityAnimation;
-    [SerializeField] private float scaleIncreaseAmount;
-    private TestGameManager _testGameManager;
+    [SerializeField] private PlayerController _otherPlayer;
     [SerializeField] private string _gameManagerName;
 
+    [Header("Ability Function")]
+    [SerializeField] private bool _specialIsActivated;
+    [SerializeField] private bool _specialWaiting;
+    [SerializeField] private GameObject _abilityAnimation;
+    [SerializeField] private float _transferCooldown;
+    [SerializeField] private float _scaleIncreaseAmount;
+
+    [Header("Movement")]
+    [SerializeField] private float _speed;
+
+    private Rigidbody2D _rigidbody;
+    private TestGameManager _testGameManager;
+
+    private bool _canTransfer;
+    
     private void Awake()
     {
-        _rb = GetComponent<Rigidbody2D>();
+        _rigidbody = GetComponent<Rigidbody2D>();
         _testGameManager = GameObject.Find(_gameManagerName).GetComponent<TestGameManager>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        canTransfer = true;
+        _canTransfer = true;
     }
 
     // Update is called once per frame
@@ -42,92 +48,90 @@ public class PlayerController : MonoBehaviour
         {
             Death();
         }
-
     }
 
     public void PlayerMovement(int index)
     {
-        float delta = speed * Time.deltaTime;
+        float delta = _speed * Time.deltaTime;
 
         if (index == 1)
         {
             float h = Input.GetAxis("Horizontal");
             float v = Input.GetAxis("Vertical");
-            _rb.velocity = new Vector2(h, v) * delta;
+            _rigidbody.velocity = new Vector2(h, v) * delta;
         }
         else
         {
             float h1 = Input.GetAxis("Horizontal1");
             float v1 = Input.GetAxis("Vertical1");
-            _rb.velocity = new Vector2(h1, v1) * delta;
+            _rigidbody.velocity = new Vector2(h1, v1) * delta;
         }
     }
 
     private void SpecialAbility(int index)
     {
-        if (index == 1 && canTransfer)
+        if (index == 1 && _canTransfer)
         {
             if (Input.GetKey(KeyCode.LeftShift))
             {
-                specialWaiting = true;
+                _specialWaiting = true;
                 print("player1 waiting");
 
-                if (otherPlayer.specialWaiting)
+                if (_otherPlayer._specialWaiting)
                 {
-                    specialIsActivated = !specialIsActivated;
-                    canTransfer = false;
-                    Invoke("CanTransfer", transferCooldown);
+                    _specialIsActivated = !_specialIsActivated;
+                    _canTransfer = false;
+                    Invoke("CanTransfer", _transferCooldown);
                 }
             }
             else
             {
-                specialWaiting = false;
+                _specialWaiting = false;
             }
         }
 
-        if (index == 2 && canTransfer)
+        if (index == 2 && _canTransfer)
         {
-            if (Input.GetKey(KeyCode.RightShift) && canTransfer)
+            if (Input.GetKey(KeyCode.RightShift) && _canTransfer)
             {
-                specialWaiting = true;
+                _specialWaiting = true;
                 print("player2 waiting");
 
-                if (otherPlayer.specialWaiting)
+                if (_otherPlayer._specialWaiting)
                 {
-                    specialIsActivated = !specialIsActivated;
-                    canTransfer = false;
-                    Invoke("CanTransfer", transferCooldown);
+                    _specialIsActivated = !_specialIsActivated;
+                    _canTransfer = false;
+                    Invoke("CanTransfer", _transferCooldown);
                 }
             }
             else
             {
-                specialWaiting = false;
+                _specialWaiting = false;
             }
         }
 
-
-        if (specialIsActivated)
+        if (_specialIsActivated)
         {
-            gameObject.transform.localScale = new Vector3(scaleIncreaseAmount, scaleIncreaseAmount, 1);
-            abilityAnimation.SetActive(true);
+            gameObject.transform.localScale = new Vector3(_scaleIncreaseAmount, _scaleIncreaseAmount, 1);
+            _abilityAnimation.SetActive(true);
         }
         else
         {
             gameObject.transform.localScale = new Vector3(1, 1, 1);
-            abilityAnimation.SetActive(false);
+            _abilityAnimation.SetActive(false);
         }
 
     }
 
     private void CanTransfer()
     {
-        canTransfer = true;
+        _canTransfer = true;
         print("Can transfer = true");
     }
 
     private void Death()
     {
-        _rb.velocity = Vector2.zero; 
+        _rigidbody.velocity = Vector2.zero; 
         //explosion
     }
 
