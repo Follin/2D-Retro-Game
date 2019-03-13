c17ed03c4906cdbcda3b500b873abe5cf66494b3
JohnWalden <jabbawalden@gmail.com>
2019-03-13 11:28:29 +0100

    Player size changes new commit

--- 2D Project FG/Assets/Scenes/Test Scenes/John/JohnPlayerTest.cs
+++ 2D Project FG/Assets/Scenes/Test Scenes/John/JohnPlayerTest.cs
@@ -8,22 +8,29 @@ public class JohnPlayerTest : MonoBehaviour
     private int _index;
     public float speed;
     Rigidbody2D _rb;
+    public JohnPlayerTest otherPlayer;
+    public bool specialIsActivated;
+    public bool specialWaiting;
+    private bool canTransfer;
+    public float transferCooldown;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
+
     }
 
     // Start is called before the first frame update
     void Start()
     {
-
+        canTransfer = true;
     }
 
     // Update is called once per frame
     void Update()
     {
         PlayerMovement(_index);
+        SpecialAbility(_index);
     }
 
     public void PlayerMovement(int index)
@@ -44,5 +51,69 @@ public class JohnPlayerTest : MonoBehaviour
         }
     }
 
+    private void SpecialAbility(int index)
+    {
+        if (index == 1 && canTransfer)
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                specialWaiting = true;
+                print("player1 waiting");
+
+                if (otherPlayer.specialWaiting)
+                {
+                    specialIsActivated = !specialIsActivated;
+                    canTransfer = false;
+                    Invoke("CanTransfer", transferCooldown);
+                }
+            }
+            else
+            {
+                specialWaiting = false;
+            }
+        }
+
+        if (index == 2 && canTransfer)
+        {
+            if (Input.GetKey(KeyCode.RightShift) && canTransfer)
+            {
+                specialWaiting = true;
+                print("player2 waiting");
+
+                if (otherPlayer.specialWaiting)
+                {
+                    specialIsActivated = !specialIsActivated;
+                    canTransfer = false;
+                    Invoke("CanTransfer", transferCooldown);
+                }
+            }
+            else
+            {
+                specialWaiting = false;
+            }
+        }
+
+
+        if (specialIsActivated)
+        {
+            gameObject.transform.localScale = new Vector3(1.5f, 1.5f, 1);
+        }
+        else
+        {
+            gameObject.transform.localScale = new Vector3(1, 1, 1);
+        }
+
+    }
+
+    private void CanTransfer()
+    {
+        canTransfer = true;
+        print("Can transfer = true");
+    }
+
+    private void Death()
+    {
+
+    }
 
 }
