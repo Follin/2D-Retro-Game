8c6360898e148a2b7d47e45c026eab6765849dd1
Olivia Follin <odm.follin@gmail.com>
2019-03-14 16:35:51 +0100

    Trigger on ultimate enemy

--- 2D Project FG/Assets/Enemy/UltimateEnemy.cs
+++ 2D Project FG/Assets/Enemy/UltimateEnemy.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class UltimateEnemy : MonoBehaviour
 {
-    public GameObject player1, player2;
+    [SerializeField] private GameObject _player1, _player2;
+
+    [SerializeField] private float _distanceToPlayer1, _distanceToPlayer2;
 
     /// <summary>
     /// Make sure to add functionality for checking player distance
     /// and feedback for when the player is close - screen darkens, particle fx - the enemy becomes happier?? Olivia you are strange...
     /// </summary>
 
-
+    private void Update()
+    {
+        _distanceToPlayer1 = _player1.transform.position.y - transform.position.y;
+        _distanceToPlayer2 = _player2.transform.position.y - transform.position.y;
+    }
 
 }
