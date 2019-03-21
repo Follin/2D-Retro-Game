8031676c9e4b0f1e859358d3b2defd63c826b8f3
Folke Stovring <Folkesto@hotmail.com>
2019-03-21 14:44:23 +0100

    Added feedback script

---
+++ 2DProjectFG/Assets/Collectables/FeedbackFlow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeedbackFlow : MonoBehaviour
+{
+    public Transform UIScoreText;
+    public Camera mainCamera;
+    public float ShrinkRate = 1f;
+    void Update()
+    {
+        transform.localScale -= (new Vector3(ShrinkRate, ShrinkRate, ShrinkRate) * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, mainCamera.ScreenToWorldPoint(UIScoreText.position), (10f * Time.deltaTime));
+        if(transform.localScale.x < 0 || Vector3.Distance(transform.position,mainCamera.ScreenToWorldPoint(UIScoreText.position)) < 0.2)
+        {
+            Destroy(this);
+        }
+    }
+}
